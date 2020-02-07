5e7721b0c65003accdc25ae4da91f0fbe61d7bbb
cahogan27 <cahogan@email.wm.edu>
2020-02-06 22:34:36 -0500

    recursive function
    look for main in folder

---
+++ cmake-build-debug/CMakeFiles/rec_fib.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/rec_fib.dir/main.cpp.o"
+  "rec_fib"
+  "rec_fib.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/rec_fib.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
